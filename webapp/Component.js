aeba79a3fb3277dc07f1492cbebea22c7dee9196
abhijain05 <minajain65@gmail.com>
2025-08-27 10:41:14 +0000

    Update from NeuroTune - 2025-08-27T10:41:14.888Z

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,26 +1,18 @@
 sap.ui.define([
-  "sap/ui/core/UIComponent",
-  "sap/ui/Device"
-], function (UIComponent, Device) {
-  "use strict";
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device"
+], function(UIComponent, Device) {
+	"use strict";
 
-  return UIComponent.extend("converted.fiscaldataview.Component", {
-    metadata: {
-      manifest: "json"
-    },
+	return UIComponent.extend("converted.fiscaldataview.Component", {
+		metadata: {
+			manifest: "json"
+		},
 
-    /**
-     * The component is initialized by UI5 automatically during the startup of the app
-     */
-    init: function () {
-      // Call the base component's init function
-      UIComponent.prototype.init.apply(this, arguments);
-
-      // Set device model
-      this.setModel(new sap.ui.model.json.JSONModel(sap.ui.Device), "device");
-
-      // Initialize the router for navigation
-      this.getRouter().initialize();
-    }
-  });
+		init: function() {
+			UIComponent.prototype.init.apply(this, arguments);
+			this.setModel(new sap.ui.model.json.JSONModel({ isTouch: Device.support.touch }), "device");
+			this.getRouter().initialize();
+		}
+	});
 });
